5fda9a66f7d1c8c889b21ea0ad07bdc763e77c18
Varun1802 <60618498+Varun1802@users.noreply.github.com>
2024-06-19 01:37:58 +0530

    get-in-touch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,13 +24,16 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:varunchoudharywork@gmail.com">
+        <Link
+          href="https://mail.google.com/mail/u/?authuser=varunchoudharywork@gamil.com"
+          target="_blank"
+        >
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
-        </a>
+        </Link>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
